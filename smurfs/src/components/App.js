79bc6d865a11e1bbf16f4299a7a4e9de8e2d0d80
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-17 08:53:45 -0700

    most of app.js complete

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
+import { getSmurphs } from './../actions/index';
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
@@ -7,16 +10,43 @@ import './App.css';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    console.log(`CDM: fetchingSmurfs`);
+    this.props.getSmurphs();
+  }
+
   render() {
+    if (this.props.fetchingSmurfs) {
+      return <div> ... Loading</div>;
+    }
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
+        {/* <div>Welcome to your Redux version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div>Have fun!</div> */}
+
+        <SmurfList smurfs={this.props.smurfs} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log(`Current state: ` + state.smurfs);
+  return {
+    smurfs: state.smurfReducer.characters,
+    fetchingSmurfs: state.smurfReducer.isLoading,
+    error: state.smurfReducer.error
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSmurphs }
+)(App);
